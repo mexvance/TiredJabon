b6c7c1fd8581462a3025863b7e99f9a5a501cae9
Michael Vance <mexvance04@gmail.com>
2019-04-01 15:04:32 -0600

    I have 2 rest apis making my Change My Mind memes

--- TiredJabon/Controllers/HomeController.cs
+++ TiredJabon/Controllers/HomeController.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using TiredJabon.Models;
 
 namespace TiredJabon.Controllers
@@ -12,7 +16,24 @@ namespace TiredJabon.Controllers
     {
         public IActionResult Index()
         {
+            var request1 = GetReleases("https://opinionated-quotes-api.gigalixirapp.com/v1/quotes?tags=short&tmode=all");
+
+            //var memes = JObject.Parse(request);
+            var jObject = JObject.Parse(request1);
+            var text0 = (string)jObject["quotes"][0]["quote"];
+        /*ViewData["memes"] =*/
+        //var data = (Memes)JsonConvert.DeserializeObject<Memes>((string)jObject["data"]["memes"]);
+        /*Random rand = new Random();
+        int num = rand.Next(jObject["quotes"]["quote"]);
+        ViewData["memes"]= (string)jObject["data"]["memes"][num]["name"];
+        ViewData["url"] = (string)jObject["data"]["memes"][num]["url"];*/
+        //ViewData["memes"] = request;
+        var url = "https://api.imgflip.com/caption_image?template_id=129242436&username=mexvance&password=Restapi1&text0="+ text0;
+            var request = GetReleases(url);
+            jObject = JObject.Parse(request);
+            ViewData["url"] = (string)jObject["data"]["url"];
             return View();
+
         }
 
         public IActionResult Privacy()
@@ -24,6 +45,16 @@ namespace TiredJabon.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public string GetReleases(string url)
+        {
+            var client = new WebClient();
+            //client.Headers.Add(RequestConstants.UserAgent, RequestConstants.UserAgentValue);
+
+            var response = client.DownloadString(url);
+
+            return response;
         }
     }
 }
---
+++ TiredJabon/Models/Memes.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TiredJabon.Models
+{
+    public class Memes
+    {
+        [JsonProperty("memes")]
+        public MemeStructure memes { get; set; }
+    }
+    public class MemeStructure
+    {
+        [JsonProperty("id")]
+        public int id { get; set; }
+        [JsonProperty("name")]
+        public int name { get; set; }
+        [JsonProperty("url")]
+        public int url { get; set; }
+        [JsonProperty("width")]
+        public int width { get; set; }
+        [JsonProperty("height")]
+        public int height { get; set; }
+        [JsonProperty("box_count")]
+        public int box_count { get; set; }
+    }
+}
